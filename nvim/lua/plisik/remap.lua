375d0e16204c63ad387d17603db79177a4339eac
Patryk Lisik <lisikparyk@gmail.com>
2024-01-28 14:52:45 +0100

    select whole buffer with leader as

--- nvim/lua/plisik/remap.lua
+++ nvim/lua/plisik/remap.lua
@@ -49,10 +49,4 @@ vim.keymap.set({"v", "n"}, "y", '"*y :let @+=@*<CR>', {noremap=true, silent=true
 vim.keymap.set( "n", "yy", '"*yy :let @+=@*<CR>', {noremap=true, silent=true})
 
 --select all
-vim.keymap.set({"n"}, "<C-a>" , "ggVG")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set({"n"}, "<leader>as" , "ggVG")
