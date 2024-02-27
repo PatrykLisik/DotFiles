b0fbbf57bc2139c98bc6cdd0d43085d7fe9af607
Patryk Lisik <lisikparyk@gmail.com>
2024-02-27 18:17:14 +0100

    fix newtre error Lex error

--- nvim/lua/plisik/remap.lua
+++ nvim/lua/plisik/remap.lua
@@ -25,9 +25,9 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 --vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
---vim.g.netrw_browse_split = 4
+vim.g.netrw_browse_split = 0
 --vim.g.netrw_altv = 1
--- vim.g.netrw_winsize = 25
+vim.g.netrw_winsize = 25
 
 vim.keymap.set("n","<leader>e", vim.cmd.Lexplore)
 
