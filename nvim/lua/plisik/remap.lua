622b06253ea51283c3eb386c6a8a9fad95997fd0
Patryk Lisik <lisikpatryk@gmail.com>
2024-07-28 12:14:56 +0200

    Reformat remap file

--- nvim/lua/plisik/remap.lua
+++ nvim/lua/plisik/remap.lua
@@ -13,7 +13,7 @@ vim.keymap.set("n", "<leader>", "<nop>")
 -- paste without replace
 vim.keymap.set("x", "p", [["_d+P]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
@@ -35,38 +35,38 @@ vim.g.netrw_winsize = 25
 --adad adadad a
 
 ---No buffer overwriting on , paste, delete, cut in normal mode is delete
-vim.keymap.set({"n", "v"},"L", "e")
-vim.keymap.set({"n","v"},"H", "b")
-vim.keymap.set({"v","n"}, "J", "j")
+vim.keymap.set({ "n", "v" }, "L", "e")
+vim.keymap.set({ "n", "v" }, "H", "b")
+vim.keymap.set({ "v", "n" }, "J", "j")
 -- LSP uses capital K to show tool tips
 --vim.keymap.set({"v", "n"}, "K", "k")
 
 
-vim.keymap.set({"v", "n"}, "p", '"*p')
+vim.keymap.set({ "v", "n" }, "p", '"*p')
 --vim.keymap.set({"n", "v"}, "pp", "*p")
-vim.keymap.set({"x", "v"}, "x", '"*x :let @+=@*<CR>', {noremap=true, silent=true})
-vim.keymap.set({"v", "n"}, "d", '"_d')
-vim.keymap.set({"v", "n"}, "D", '_d$', {noremap=true, silent=true})
-vim.keymap.set({"v", "n"}, "y", '"*y :let @+=@*<CR>', {noremap=true, silent=true})
-vim.keymap.set( "n", "yy", '"*yy :let @+=@*<CR>', {noremap=true, silent=true})
+vim.keymap.set({ "x", "v" }, "x", '"*x :let @+=@*<CR>', { noremap = true, silent = true })
+vim.keymap.set({ "v", "n" }, "d", '"_d')
+vim.keymap.set({ "v", "n" }, "D", '_d$', { noremap = true, silent = true })
+vim.keymap.set({ "v", "n" }, "y", '"*y :let @+=@*<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "yy", '"*yy :let @+=@*<CR>', { noremap = true, silent = true })
 
 --select all
-vim.keymap.set({"n"}, "<leader>as" , "ggVG")
+vim.keymap.set({ "n" }, "<leader>as", "ggVG")
 
 --moves in insert mode
-vim.keymap.set("i","<C-h>", "<Left>", {noremap=true})
-vim.keymap.set("i","<C-j>", "<Down>", {noremap=true})
-vim.keymap.set("i","<C-k>", "<Up>", {noremap=true})
-vim.keymap.set("i","<C-l>", "<Right>", {noremap=true})
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
 
 --ctrl backspace to detele whole word
-vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', { noremap = true })
 
---moving to different buferrs fast 
-vim.api.nvim_set_keymap('n', '<leader>l', '<C-W>l', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>h', '<C-W>h', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>j', '<C-W>j', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>k', '<C-W>k', {noremap=true})
+--moving to different buferrs fast
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-W>l', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-W>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-W>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-W>k', { noremap = true })
 
 -- vim.cmd("Spell")
 
