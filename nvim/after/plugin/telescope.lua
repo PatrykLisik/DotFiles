23859e1f0c900f963b84ce683b66be92339c7051
Patryk Lisik <lisikpatryk@gmail.com>
2024-09-25 18:37:39 +0200

    more telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,7 +5,7 @@ require('telescope').setup {
             ".git/",
             "%.env"
         },
-
+ initial_mode = "normal"
     },
     extensions = {
         fzf = {
@@ -44,19 +44,29 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tr', builtin.treesitter, {})
+vim.keymap.set('n', 'z=', builtin.spell_suggest, { noremap = true })
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, { noremap = true })
+vim.keymap.set('n', 'lr', builtin.lsp_references, { noremap = true })
+vim.keymap.set('n', 'li', builtin.lsp_implementations, { noremap = true })
+
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 --https://www.reddit.com/r/neovim/comments/p8wtmn/comment/hx0usg8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 vim.keymap.set("v", "<leader>fg", "\"zy<cmd>exec 'Telescope grep_string default_text=' . escape(@z, ' ')<cr>")
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>sh', builtin.search_history, {})
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>re', builtin.registers, {})
 
 require("telescope").load_extension "file_browser"
 
 -- open file_browser with the path of the current buffer
-vim.keymap.set("n", "<space>bf", ":Telescope file_browser path=%:p:h select_buffer=true<CR><ESC>")
+vim.keymap.set("n", "<leader>bf", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 
 -- Alternatively, using lua API
-vim.keymap.set("n", "<space>ba", function()
+vim.keymap.set("n", "<leader>ba", function()
     require("telescope").extensions.file_browser.file_browser()
 end)
 --
