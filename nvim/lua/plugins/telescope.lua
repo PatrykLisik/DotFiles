6f3623a40fbbf52a69ecebac438871dbcc165c1d
Patryk Lisik <lisikpatryk@gmail.com>
2024-11-20 20:53:21 +0100

    refactoring + surround

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -87,7 +87,6 @@ return {
 
         local builtin = require('telescope.builtin')
 
-
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true })
         vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>tr', builtin.resume, {})
