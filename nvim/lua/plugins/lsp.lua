e3fd5475b4bca809803f82325fe12e6b72a7fff4
Patryk Lisik <lisikpatryk@gmail.com>
2025-04-07 18:15:16 +0200

    no typos lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -205,7 +205,7 @@ return {
                     -- "vale_ls",
                     "docker_compose_language_service",
                     "dockerls",
-                    "typos_lsp",
+                    -- "typos_lsp",
                     "ltex",
                     "jsonls",
                     "yamlls",
@@ -334,19 +334,19 @@ return {
                 filetypes = { "Dockerfile" }
             }
 
-            require('lspconfig').typos_lsp.setup({
-                filetypes = { "lua", "go", "python", "yaml", "java", "sql" },
-                -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
-                cmd_env = { RUST_LOG = "error" },
-                init_options = {
-                    -- Custom config. Used together with any workspace config files, taking precedence for
-                    -- settings declared in both. Equivalent to the typos `--config` cli argument.
-                    config = '~/.config/typos_lsp/typos.toml',
-                    -- How typos are rendered in the editor, eg: as errors, warnings, information, or hints.
-                    -- Defaults to error.
-                    diagnosticSeverity = "Warning"
-                }
-            })
+            -- require('lspconfig').typos_lsp.setup({
+            --     filetypes = { "lua", "go", "python", "yaml", "java", "sql" },
+            --     -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+            --     cmd_env = { RUST_LOG = "error" },
+            --     init_options = {
+            --         -- Custom config. Used together with any workspace config files, taking precedence for
+            --         -- settings declared in both. Equivalent to the typos `--config` cli argument.
+            --         config = '~/.config/typos_lsp/typos.toml',
+            --         -- How typos are rendered in the editor, eg: as errors, warnings, information, or hints.
+            --         -- Defaults to error.
+            --         diagnosticSeverity = "Warning"
+            --     }
+            -- })
 
             local util = require 'lspconfig.util'
             require('lspconfig').gopls.setup {
