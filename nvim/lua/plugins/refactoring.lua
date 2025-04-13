2420c43cb80ddc4494e2d7a882d9b03536d93735
Patryk Lisik <lisikpatryk@gmail.com>
2025-04-13 20:24:59 +0200

    refactoring improved

--- nvim/lua/plugins/refactoring.lua
+++ nvim/lua/plugins/refactoring.lua
@@ -7,13 +7,28 @@ return {
     keys = {
         {
             "<leader>rr",
-            function() require('telescope').extensions.refactoring.refactors() end,
+            function() require('refactoring').select_refactor() end,
             mode = { "n", "x" }
+        },
+        {
+            "<leader>rp",
+            function() require('refactoring').debug.printf({ below = true }) end
+        },
+        {
+            "<leader>pp",
+            function() require('refactoring').debug.print_var() end,
+            mode = { "n", "x" }
+        },
+        {
+            "<leader>rc",
+            function() require('refactoring').debug.cleanup({}) end
         }
     },
-    config = function()
-        require("refactoring").setup()
-        require("telescope").load_extension("refactoring")
-    end
-
+    opts = {
+        print_var_statements = {
+            python = {
+                'print(f"XXX %s {%s=}")'
+            }
+        }
+    }
 }
