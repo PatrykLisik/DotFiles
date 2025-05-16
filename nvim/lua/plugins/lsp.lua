eb923f85dc41e09ae756e9b6f274ac68f98d9a2f
Patryk Lisik <lisikpatryk@gmail.com>
2025-05-16 21:26:05 +0200

    inlay hints

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -177,6 +177,7 @@ return {
                 desc = 'LSP actions',
                 callback = function(event)
                     local opts = { buffer = event.buf }
+                    vim.lsp.inlay_hint.enable(true)
 
                     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
                     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
