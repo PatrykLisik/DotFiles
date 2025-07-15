c56ad3b6bbb1687e2a627f02d85019423e1baab8
Patryk Lisik <lisikpatryk@gmail.com>
2025-07-15 20:41:54 +0200

    Orgiami(fold plugin) is set to default settings

--- nvim/lua/plugins/origiami.lua
+++ nvim/lua/plugins/origiami.lua
@@ -1,11 +1,5 @@
 return {
-	"chrisgrieser/nvim-origami",
-	event = "VeryLazy",
-	opts = {
-	foldtextWithLineCount = {
-		enabled = package.loaded["ufo"] == nil,
-		template = "  󰁂  %s", -- `%s` gets the number of folded lines
-		hlgroupForCount = "Comment",
-	},
-    }, -- needed even when using default config
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {},
 }
